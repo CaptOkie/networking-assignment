566c7c96e99846d33c3c04caa54b35c18350ad82
Elephly <elephly88@gmail.com>
2015-09-30 21:20:41 -0400

    Handling of abrupt disconnect
    Also, github practically forced me to plop in those .gitignores

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -4,12 +4,59 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 
 import client.ctrl.Controller;
+import common.ui.Console;
 
 public class Client {
 
+	static boolean run;
+	static boolean err;
+	static Console console;
+	
     public static void main(final String[] args) throws UnknownHostException, IOException, ClassNotFoundException { // TODO Maybe handle these exceptions
-        try (final Controller controller = new Controller()) {
-            controller.run();
-        }
+    	run = true;
+    	console = new Console();
+    	
+    	while (run) {
+    		err = false;
+	    	try (final Controller controller = new Controller()) {
+	            controller.run();
+    			controller.close();
+	        }
+	    	catch (UnknownHostException e) {
+	    		err = true;
+	    		console.writeLine("UnknownHostException: " + e.getLocalizedMessage());
+	    	}
+	    	catch (IOException e) {
+	    		err = true;
+	    		console.writeLine("IOException: " + e.getLocalizedMessage());
+	    	}
+	    	catch (ClassNotFoundException e) {
+	    		err = true;
+	    		console.writeLine("ClassNotFoundException: " + e.getLocalizedMessage());
+	    	}
+	    	finally {
+	    		if (err) {
+	    			do {
+	    				err = false;
+	    				String quit = console.readLine("Would you like to quit [Y/n]? ").toLowerCase();
+	    				switch (quit) {
+	    				case "y":
+	    				case "yes":
+	    					run = false;
+	    					break;
+	    				case "n":
+	    				case "no":
+	    					break;
+	    				default:
+	    					err = true;
+	    					console.writeLine("Invalid input.");
+	    					break;
+	    				}
+	    			} while (err);
+	    		}
+	    	}
+    	}
+    	
+    	console.close();
     }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,15 +2,27 @@ package server;
 
 import java.io.IOException;
 
+import common.ui.Console;
 import server.ctrl.Controller;
 
 public class Server {
 
-    public static void main(final String[] args) throws IOException, ClassNotFoundException { // TODO Maybe handle these exceptions
+	static Console console;
 
-        System.out.println("Listening");
-        try (final Controller controller = new Controller()) {
-            controller.run();
-        }
+    public static void main(final String[] args) throws IOException, ClassNotFoundException { // TODO Maybe handle these exceptions
+    	console = new Console();
+    	
+    	while (true) {
+	        try (final Controller controller = new Controller()) {
+	            console.writeLine("Listening");
+	            controller.run();
+	        }
+	        catch (IOException e) {
+	        	console.writeLine("IOException: " + e.getLocalizedMessage());
+	        }
+	        catch (ClassNotFoundException e) {
+	        	console.writeLine("ClassNotFoundException: " + e.getLocalizedMessage());
+	        }
+    	}
     }
 }
